a9d8b1fc54e2879d514520faffce5f86f178cb7b
brettl1991 <brettl1991@gmail.com>
2024-02-03 17:46:32 +0000

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
+import LoadMore from "@/components/LoadMore/LoadMore";
+import Loader from "@/components/Loader/Loader";
+import PokemonList from "@/components/PokemonList/PokemonList";
+import SearchBar from "@/components/SearchBar/SearchBar";
 import localFont from "next/font/local";
+import { Suspense } from "react";
 
 const pokemonFont = localFont({
   src: "fonts/Pokemon Solid.ttf",
 });
 
-export default function Home() {
+interface HomeProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const search =
+    typeof searchParams.search === "string" ? searchParams.search : undefined;
+
   return (
     <div>
-      <div className="flex justify-center items-start p-8 bg-[url('/images/bg-title.png')] bg-contain bg-center bg-repeat-x sm:p-4">
+      <div className="flex justify-center items-start bg-[url('/images/bg-title.png')] bg-contain bg-center bg-repeat-x sm:p-4">
         <h1
           className={`text-center text-5xl font-bold text-[#fbc418] ${pokemonFont.className} title`}
         >
           Choose your Pokémon
         </h1>
       </div>
-      hello
+      <div className="p-6 md:px-10 lg:px-40">
+        <div className="flex flex-col items-center space-y-4 max-w-max mx-auto">
+          <SearchBar search={search} />
+          <Suspense fallback={<Loader />} key={search}>
+            <PokemonList query={search} />
+          </Suspense>
+          {!search && <LoadMore />}
+        </div>
+      </div>
     </div>
   );
 }
