f2b5ac76b9b56b5b2a9b1009f9bca6994027ee0e
brettl1991 <brettl1991@gmail.com>
2024-02-05 11:23:43 +0000

    fix styles for links

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -1,8 +1,8 @@
-import { Pokemon } from '@/types';
-import Image from 'next/image';
-import Link from 'next/link';
-import BadgeType from '../BadgeType/BadgeType';
-import { getColorsByPokemonType } from '@/utils';
+import { Pokemon } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import BadgeType from "../BadgeType/BadgeType";
+import { getColorsByPokemonType } from "@/utils";
 
 interface PokemonItemProps {
   pokemon: Pokemon;
@@ -10,9 +10,9 @@ interface PokemonItemProps {
 
 export default function PokemonItem({ pokemon }: PokemonItemProps) {
   const imageUrl =
-    pokemon.sprites.other['official-artwork'].front_default ||
+    pokemon.sprites.other["official-artwork"].front_default ||
     pokemon.sprites.front_default ||
-    '/images/placeholder.png';
+    "/images/placeholder.png";
 
   const type = pokemon.types[0].type.name;
   const bgColor = getColorsByPokemonType(type).backgroundColor;
@@ -25,9 +25,9 @@ export default function PokemonItem({ pokemon }: PokemonItemProps) {
         {imageUrl && (
           <div
             className={`w-24 h-24 ${
-              imageUrl === '/images/placeholder.png'
-                ? 'flex items-center justify-center overflow-hidden'
-                : ''
+              imageUrl === "/images/placeholder.png"
+                ? "flex items-center justify-center overflow-hidden"
+                : ""
             }`}
           >
             <Image
@@ -36,12 +36,17 @@ export default function PokemonItem({ pokemon }: PokemonItemProps) {
               width={156}
               height={156}
               className="object-cover"
+              layout="responsive"
               unoptimized
             />
           </div>
         )}
-        <span className="text-black text-sm">#{pokemon.id}</span>
-        <h2 className="mt-2 text-lg font-bold capitalize">{pokemon.name}</h2>
+        <span className="text-black text-sm no-underline no-underline">
+          #{pokemon.id}
+        </span>
+        <h2 className="mt-2 text-lg font-bold capitalize no-underline">
+          {pokemon.name}
+        </h2>
         <div className="flex items-center justify-center mt-2 gap-1">
           {pokemon.types.map((type, index) => (
             <BadgeType type={type.type.name} key={index} />
